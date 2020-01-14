54bc628b48c2180081dc0f45166d62806335875a
陈帅 <1301491593@qq.com>
2020-01-14 09:22:06 +0800

    'add'

--- front/admin/src/layout/components/Sidebar/SidebarItem.vue
+++ front/admin/src/layout/components/Sidebar/SidebarItem.vue
@@ -1,16 +1,37 @@
 <template>
-  <div v-if="!item.hidden">
+  <div v-if="!item.hidden" class="menu-wrapper">
     <template v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
       <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
-        <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
-          <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="onlyOneChild.meta.title" />
+        <el-menu-item ref="menuItem" :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
+          <!--<item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="onlyOneChild.meta.title" />-->
+          <item
+            :icon="item.path === choseBaseRouter?(item.meta&&'chose-' + item.meta.icon) || 'chose-' + onlyOneChild.meta.icon:onlyOneChild.meta.icon||(item.meta&&item.meta.icon)"
+            :title="onlyOneChild.meta.title"
+          />
         </el-menu-item>
       </app-link>
     </template>
 
-    <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+    <!--<el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
       <template slot="title">
         <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title" />
+      </template>
+      <sidebar-item
+        v-for="child in item.children"
+        :key="child.path"
+        :is-nest="true"
+        :item="child"
+        :base-path="resolvePath(child.path)"
+        class="nest-menu"
+      />
+    </el-submenu>-->
+    <!--如果有子菜单-->
+    <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+      <template slot="title">
+        <item
+          :icon="item.path === choseBaseRouter?(item.meta&&'chose-' + item.meta.icon) || 'chose-' + item.meta.icon:item.meta.icon"
+          :title="item.meta.title"
+        />
       </template>
       <sidebar-item
         v-for="child in item.children"
@@ -46,6 +67,10 @@ export default {
       default: false
     },
     basePath: {
+      type: String,
+      default: ''
+    },
+    choseBaseRouter: {
       type: String,
       default: ''
     }
@@ -93,3 +118,44 @@ export default {
   }
 }
 </script>
+<style>
+  /*.el-popover{*/
+  /*  padding: 10px!important;*/
+  /*  font-size: 13px!important;*/
+  /*}*/
+  .el-menu-item.submenu-title-noDropdown{
+    padding-left: 10px!important;
+  }
+  .el-submenu__title{
+    padding-left: 10px!important;
+    padding-right: 0;
+  }
+  .el-submenu__title i{
+    color: #ffffff;
+    right: 5px;
+  }
+  .el-submenu__icon-arrow{
+    margin-top: -5px!important;
+  }
+  .el-menu--vertical .el-menu{
+    background-color: #ffffff!important;
+    box-shadow: 2px 2px 2px 0 rgba(217,217,217,0.50);
+  }
+  .el-menu--vertical .nest-menu .el-menu-item{
+    background-color: #ffffff!important;
+    color: #333333!important;
+    font-size: 13px!important;
+  }
+  .el-menu--vertical .nest-menu .el-menu-item:hover,
+  .el-menu--vertical .nest-menu .el-menu-item:focus{
+    background-color: #D8F1FF!important;
+  }
+</style>
+<style lang="scss" scoped>
+  .menu-children-div{
+    width: 100%;
+    height: 56px;
+    font-size: 14px;
+    color: white;
+  }
+</style>
